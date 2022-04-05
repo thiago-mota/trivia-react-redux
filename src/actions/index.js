9dbffe8107b86cde53da730090b749577d864949
Kaue Alves <kaue_alves00@yahoo.com>
2022-04-05 20:41:48 -0300

    corrigindo outro erro de lint requisito 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export const SAVE_TOKEN = 'SAVE_TOKEN';
-export const PLAYER_DATA = 'PLAYER_DATA'
+export const PLAYER_DATA = 'PLAYER_DATA';
 
 export const saveToken = (payload) => ({ type: SAVE_TOKEN, payload });
-export const savePlayer = (email, name) => ({ type: PLAYER_DATA, email, name })
+export const savePlayer = (email, name) => ({ type: PLAYER_DATA, email, name });
